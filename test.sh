f23afd300ddc3589feacb09dbf257c587ba5b5eb
zzak <zzakscott@gmail.com>
2023-03-11 11:13:21 +0900

    Rewrite in Ruby

---
+++ test.sh
@@ -0,0 +1,10 @@
+export GITHUB_REPOSITORY="zzak/action-discord"
+export GITHUB_REF_NAME="ruby"
+export GITHUB_SHA="910212d0ee477b2a1e1ec973dc1b7885d03e5d05"
+export GITHUB_RUN_ID="4389628661"
+export GITHUB_WORKFLOW="zomg"
+export GITHUB_ACTOR="zzak"
+
+export ACTION_DISCORD_STATUS="failure"
+
+bundle exec ruby main.rb
